39af7208a008ad2a1800a5c9b49f972e5be6ce73
frank_shen <frank_shen@trendmicro.com>
2022-03-15 17:33:58 +0800

    feat: update sth

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+console.log("path :>> ", path);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.js"),
+      name: "MyLib",
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "React",
+        },
+      },
+    },
+  },
+});
